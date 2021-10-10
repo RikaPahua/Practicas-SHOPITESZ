49350e87e7c53802e7a64a3befe43bd03f2c75a8
RikaPahua <mvaca413@accitesz.com>
2021-10-10 11:53:24 -0500

    Creación del archivo registrarProducto

--- controlador/app.py
+++ controlador/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__, template_folder='../vista')
 
 @app.route('/')
@@ -8,6 +8,15 @@ def inicio():
 @app.route('/consultarProductos')
 def consultarProductos():
     return render_template('Productos/consultarProductos.html')
+
+@app.route('/registrarProducto')
+def registrarProducto():
+    return render_template('Productos/registrarProducto.html')
+
+@app.route('/registrarNuevoProducto',methods=['post'])
+def registrarNuevoProducto():
+    nombre = request.form['nombre']
+    return 'Se a registrado el producto: '+nombre
 
 if __name__ == '__main__':
     app.run(debug=True)
